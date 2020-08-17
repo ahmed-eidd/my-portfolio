08382cc3078b0c36e955e6392c6fb5a52d900526
Ahmed Eid <ahmedeid.webdev@gmail.com>
2020-08-17 04:49:56 +0200

    added about with some g styling

---
+++ src/components/About/About.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import classes from './About.module.css';
+
+const About = () => {
+  return (
+    <section className={classes.About} id='about'>
+      <div className={classes.AboutMe}>
+        <div className={classes.AboutMeText}>
+          <h4>Hi! I'm Ahmed</h4>
+          <p>
+            My name is Ahmed Eid a Front End Developer based in Cario,Egypt.
+            Like many other people, I enjoy spending my free time increasing my
+            knowledge of Web Development and building new and challenging
+            projects using React,Redux,Gatsby and many Front end Technologies
+          </p>
+        </div>
+        <div className={classes.AboutMeSkills}>
+          <h3>Skills</h3>
+          <ul>
+            <li>
+              <i class="fab fa-html5"></i> HTML
+            </li>
+            <li>
+              <i class="fab fa-css3-alt"></i> CSS
+            </li>
+            <li>
+              <i class="fab fa-sass"></i> Sass
+            </li>
+            <li>
+              <i class="fab fa-js-square"></i> Javascript
+            </li>
+            <li>
+              <i class="fab fa-react"></i> React
+            </li>
+            <li>
+              <i class="fas fa-file-code"></i> Redux
+            </li>
+            <li><i class="fab fa-github"></i>
+              Github
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={classes.ContactMe}>
+        <h3>Contact Me</h3>
+       
+          <ul className={classes.ContactMeList}>
+            <li className={classes.ContactMeItem}><i class="fas fa-envelope"></i><p>Ahmedeid.webdev@gmail.com</p></li>
+            <li className={classes.ContactMeItem}> <i class="fab fa-github"></i>  <a target='_blank' href="https://github.com/ahmed-eidd">Github</a></li>
+            <li className={classes.ContactMeItem}><i class="fab fa-linkedin"></i> <a target='_blank' href="https://www.linkedin.com/in/ahmed-eidd/">Linkedin</a></li>
+            <li className={classes.ContactMeItem}><i class="far fa-file-alt"></i><a target='_blank' href="">Resume</a></li>
+          </ul>
+      
+      </div>
+    </section>
+  );
+};
+
+export default About;
