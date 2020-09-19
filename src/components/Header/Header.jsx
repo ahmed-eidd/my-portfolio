896a460ad09f871f5d3362d2ecdf44126200624e
Ahmed Eid <ahmedeid.webdev@gmail.com>
2020-09-19 03:02:29 +0200

    add cssplugin to fix the bug in header in prod

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,51 @@
 import React, { useEffect, useRef } from 'react';
-import { TimelineLite, TweenMax, Power1 } from 'gsap';
+import { TimelineLite, CSSPlugin, Power1 } from 'gsap';
 import classes from './Header.module.css';
 import Btn from '../UI/Button/Button';
 import { Link } from 'react-scroll';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
+// eslint-disable-line
+const C = CSSPlugin;
+
 const Header = () => {
-  let slid1 = useRef(null)
-  let slid2 = useRef(null)
-  let header = useRef(null)
-  let tl = new TimelineLite()
+  let slid1 = useRef(null);
+  let slid2 = useRef(null);
+  let header = useRef(null);
+  let tl = new TimelineLite(); 
   useEffect(() => {
     Aos.init({
       duration: 1800,
     });
 
-    tl.to(slid1, 1,{ css:{y:'-100%', ease:Power1.easeOUt}})
-    tl.to(slid2, 1, { css:{y: '-100%', ease: Power1.easeOut}}, 0.3)
-    tl.to(header,0.5, {css:{opacity: '1', ease: Power1.easeIn}}, 1)
-
+    tl.to(slid1,{ duration: 1,  css: { y: '-100%', ease: Power1.easeOUt }});
+    tl.to(
+      slid2,
+      
+      { duration: 1, css: { y: '-100%', ease: Power1.easeOut } },
+      0.3
+    );
+    tl.to(
+      header,
+     
+      {duration:0.5, css: { opacity: '1', ease: Power1.easeIn } },
+      1
+    );
   }, []);
   return (
     <React.Fragment>
       <header id="home" className={classes.header}>
         {' '}
-        <div className={classes.heroHeaderContainer} ref={el => header = el}>
-          <h1 data-aos="fade-up" className={classes.headerHero} data-aos-duration="2000">
+        <div
+          className={classes.heroHeaderContainer}
+          ref={(el) => (header = el)}
+        >
+          <h1
+            data-aos="fade-up"
+            className={classes.headerHero}
+            data-aos-duration="2000"
+          >
             Hi,
           </h1>
           <h1
@@ -51,8 +70,10 @@ const Header = () => {
           </div>
         </div>
       </header>{' '}
-      <div className={classes.slider1} ref={ el => slid1 = el}><h1>Welcome</h1></div>
-      <div className={classes.slider2} ref={ el => slid2 = el}></div>
+      <div className={classes.slider1} ref={(el) => (slid1 = el)}>
+        <h1>Welcome</h1>
+      </div>
+      <div className={classes.slider2} ref={(el) => (slid2 = el)}></div>
     </React.Fragment>
   );
 };
